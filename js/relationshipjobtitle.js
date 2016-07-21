2f6ccd8b58dd37632ce9ad5be59842daf9ab9e77
Allen Shaw <allen@JoineryHQ.com>
2016-07-21 16:28:08 -0500

    Bug-fix: job titles sometimes being added twice, or wrongly.

--- js/relationshipjobtitle.js
+++ js/relationshipjobtitle.js
@@ -1,16 +1,20 @@
 /**
  * JavaScript code for com.joineryhq.relationshipjobtitle extension.
  */
-
 CRM.$(document).ready(function($){
-  // Monitor relationships tables for re-drawing by DataTables
-  CRM.$('table.crm-contact-relationship-selector-past, table.crm-contact-relationship-selector-current').on( 'draw.dt', function (e, settings) {
-    // Only perform these actions for the "Current" relationships table.
-    if ($(this).hasClass('crm-contact-relationship-selector-current')) {
-      for (i in CRM.vars.relationshipjobtitle.relationship_job_titles) {
-        var trId = 'relationship-'+ i
-        $(this).find('tr#' + trId + '>td.crm-contact-relationship-type').append('<div><em>(' + CRM.vars.relationshipjobtitle.relationship_job_titles[i] + ')</em></div>')
-      }
+  // Monitor relationships table for re-drawing by DataTables; we can't simply
+  // use $.ready() here, because DataTables re-draws the table on every page
+  // load, after (or as part of) $.ready().
+  CRM.$('table.crm-contact-relationship-selector-current').one( 'draw.dt', function (e, settings) {
+    for (i in CRM.vars.relationshipjobtitle.relationship_job_titles) {
+      var trId = 'relationship-'+ i
+      $(this).find('tr#' + trId + '>td.crm-contact-relationship-type div.relationshipjobtitle-jobtitle').remove();
+      $(this).find('tr#' + trId + '>td.crm-contact-relationship-type').append('<div class="relationshipjobtitle-jobtitle"><em>(' + CRM.vars.relationshipjobtitle.relationship_job_titles[i] + ')</em></div>')
     }
+    // Empty the relationship_job_titles object, now that it has been used.
+    // Otherwise, the stale contents of this object may populate job titles
+    // incorrectly when the page is "loaded" via AJAX, e.g., when editing
+    // a relationship.
+    CRM.vars.relationshipjobtitle.relationship_job_titles = {}
   });
 });
